7ae8867c7e2482802be6b79ed872b977b8470e7f
JewelSD <117885280+JewelSD@users.noreply.github.com>
2023-06-05 15:27:38 +0530

    Add files via upload

---
+++ lab_2/sortch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CS{
+	
+	public static void main(String[] args){
+		Scanner read=new Scanner(System.in);
+		System.out.println("Enter the String Want to sort");
+		String strin=read.nextLine();
+		char words[]=strin.toCharArray();
+		Arrays.sort(words);
+		System.out.println(new String(words));
+
+	}
+}
